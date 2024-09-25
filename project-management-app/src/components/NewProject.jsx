aebf3ffa4c7b2f133b64057cf8912aa6f9fec67d
waplak <pamitha99@gmail.com>
2024-09-25 16:41:46 +1000

    add components for project management app

---
+++ project-management-app/src/components/NewProject.jsx
@@ -0,0 +1,73 @@
+import { useRef } from "react";
+import Input from "./Input";
+import Modal from "./Modal";
+
+export default function NewProject({ onAdd, onCancel }) {
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const dueDateRef = useRef();
+  const modal = useRef();
+
+  function handleSave() {
+    const enteredTitle = titleRef.current.value;
+    const enteredDescription = descriptionRef.current.value;
+    const enteredDueDate = dueDateRef.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
+  return (
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">
+          Invalid Inputs
+        </h2>
+        <p className="text-stone-600 mb-4">
+          Oops... Looks like you fogot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              onClick={onCancel}
+              className="text-stone-800 hover:text-stone-950"
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={titleRef} label="Title" />
+          <Input ref={descriptionRef} label="Description" isTextArea={true} />
+          <Input type="date" ref={dueDateRef} label="Due Date" />
+        </div>
+      </div>
+    </>
+  );
+}
